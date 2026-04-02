27c2b9557e4176347d613d7797d4c368e6b064a9
halfthew0rldaway <wisnuwidyapradana2004@gmail.com>
2026-04-02 21:15:40 +0700

    1

--- app/projects/team/page.tsx
+++ app/projects/team/page.tsx
@@ -1,10 +1,1 @@
-import TeamClient from "./TeamClient";
-
-export const metadata = {
-  title: "Team | Zendo",
-  description: "Manage your team members and roles.",
-};
-
-export default function TeamPage() {
-  return <TeamClient />;
-}
+export default function TeamPage() { return null; }
